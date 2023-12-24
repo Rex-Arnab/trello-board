9db756deb4ce45a76c1422af5e8afdcaa6e2ed96
Arnab Biswas <khanp4397@gmail.com>
2023-12-25 02:21:00 +0530

    Add new dependencies and update file structure

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -5,13 +5,14 @@ import { InputType, ReturnType } from "./types";
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
+import { CreateBoard } from "./schema";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId } = auth();
 
   if (!userId) {
     return {
-      error: "You must be logged in to create a board",
+      error: "You must be logged in to create a board"
     };
   }
 
@@ -22,17 +23,17 @@ const handler = async (data: InputType): Promise<ReturnType> => {
   try {
     board = await db.board.create({
       data: {
-        title,
-      },
+        title
+      }
     });
   } catch (error) {
     return {
-      error: "Failed to Create Board",
+      error: "Failed to Create Board"
     };
   }
 
-  revalidatePath(`/boards/${board.id}`);
+  revalidatePath(`/board/${board.id}`);
   return { data: board };
 };
 
-export const createBoard = createSafeAction(createBoard, handler);
+export const createBoard = createSafeAction(CreateBoard, handler);
--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -1,14 +1,14 @@
 import { z } from "zod";
 
-const CreateBoard = z.object({
+export const CreateBoard = z.object({
   title: z
     .string({
       required_error: "Title is required",
-      invalid_type_error: "Title must be a string",
+      invalid_type_error: "Title must be a string"
     })
     .min(3, {
-      message: "Title must be at least 3 characters long",
-    }),
+      message: "Title must be at least 3 characters long"
+    })
 });
 
-export type CreateBoardInput = z.infer<typeof CreateBoard>;
+// export type CreateBoardInput = z.infer<typeof CreateBoard>;
